64b33c5fd5ea2a7a800d2ed8945f7d45ebd08997
Kitybot <nick34goalie@yandex.ru>
2023-10-04 21:52:35 +0300

    Включение cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,14 +5,8 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: [
-        'http://kpd.kirbob.nomoredomains.nomoredomainsrocks.ru',
-        'https://kpd.kirbob.nomoredomains.nomoredomainsrocks.ru',
-      ],
-    },
-  });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.useGlobalPipes(
     new ValidationPipe({
